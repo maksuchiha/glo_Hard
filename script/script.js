637176556362923e32ed1e6eafbb1c90f99f1574
Maksim Sheko <uchihaboy95@gmail.com>
2021-11-29 19:39:12 +0300

    feature lesson03

--- script/script.js
+++ script/script.js
@@ -1,5 +1,7 @@
+'use strict'
+
 const hard = () => {
-    const output = (item) => {
+    const lesson02 = (item) => {
         const num = item,
             output = [],
             sNumber = num.toString();
@@ -9,7 +11,51 @@ const hard = () => {
         const multiple = output.reduce((acc, rec) => acc * rec) ** 3;
         return  String(multiple).slice(0,2);
     }
-    console.log(output(266219));
+
+    console.log(lesson02(266219));
+
+
+    const lesson03 = () => {
+        let lang = prompt('Введите либо "ru" либо "en"');
+
+        if (lang === 'en') {
+            console.log('Monday, Tuesday, Wednesday, Thursday, Friday, Saturday, Sunday');
+        }   else if (lang === 'ru') {
+            console.log('Понедельник, Вторник, Среда, Четверг, Пятница, Суббота, Воскресенье');
+        }   else {
+            alert('ошибка');
+        }
+
+        switch(lang) {
+            case 'en':
+                console.log('Monday, Tuesday, Wednesday, Thursday, Friday, Saturday, Sunday');
+                break;
+
+            case 'ru':
+                console.log('Понедельник, Вторник, Среда, Четверг, Пятница, Суббота, Воскресенье');
+                break;
+
+            default:
+                console.log('ошибка');
+        }
+
+        let days = new Map([
+            ['en', ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday'] ],
+            ['ru', ['Понедельник', 'Вторник', 'Среда', 'Четверг', 'Пятница', 'Суббота', 'Воскресенье'] ]
+        ]) ;
+
+        console.log(days.get('en'));
+
+        let namePerson = prompt('Введите имя');
+
+        namePerson === 'Артём' ?
+            console.log('директор') :
+            namePerson === 'Александр' ?
+                console.log('преподаватель') :
+                console.log('студент');
+    }
+
+    lesson03()
 }
 
 hard()
