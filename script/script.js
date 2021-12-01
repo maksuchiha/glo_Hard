cfa47bfffc9728c3c04cf57f662d85f1e8dc174f
Maksim Sheko <uchihaboy95@gmail.com>
2021-12-01 23:27:07 +0300

    fix part1 and part2 in lesson05

--- script/script.js
+++ script/script.js
@@ -1,39 +1,22 @@
 'use strict'
 
 const lesson05 = () => {
-    const part1 = (item) => {
-        const isNumber = (num) => {
-            return !isNaN(parseFloat(num)) && isFinite(num)
-        }
-
-        do {
-            item = prompt('Введите число')
-        } while (!isNumber(item))
-
-        item = +item.split(' ').join('');
-
-        return item
-    }
-
-    console.log(part1())
-
     const part2 = () => {
         let arr = [ '15', '24', '56', '42', '87', '31', '55' ]
         arr.forEach((item) => {
             if (item.startsWith('2') || item.startsWith('4')) {
-                console.log(item);
+                console.log(item)
             }
         })
 
-        const isPrime = (num) => {
-            for (let i = 2; num > i; i++) {
-                if (num % i === 0) {
-                    return false;
+        let num = 100
+        nPrime:
+            for (let i = 2; i <= num; i++) {
+                for (let j = 2; j < i; j++) {
+                    if (i % j === 0) continue nPrime
                 }
+                console.log(i)
             }
-            return num > 1;
-        }
-        console.log(arr.filter(isPrime));
     }
 
     part2()
