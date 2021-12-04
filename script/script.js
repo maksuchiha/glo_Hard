df6478df49fa0b664d83a967512308c14374a8e1
Maksim Sheko <uchihaboy95@gmail.com>
2021-12-05 02:17:41 +0300

    style days in lesson07

--- script/script.js
+++ script/script.js
@@ -5,11 +5,17 @@ let days = ["Понедельник", "Вторник", "Среда", "Четв�
 let dayOfWeek = new Date().getDay()
 if (dayOfWeek === 0) dayOfWeek = 7
 for (let i = 0; i < days.length; i++) {
-    (i === dayOfWeek - 1 && i === 5) ? document.body.insertAdjacentHTML("beforeend", "<div><b>" + `<i>${days[i]}</i>` + "</b></div>") :
-        (i === dayOfWeek - 1 && i === 6) ? document.body.insertAdjacentHTML("beforeend", "<div><b>" + `<i>${days[i]}</i>` + "</b></div>") :
-            (i === dayOfWeek - 1 && (i !== 5 && i !== 6)) ? document.body.insertAdjacentHTML("beforeend", "<div><b>" + days[i] + "</b></div>") :
-                (i === 5) ? document.body.insertAdjacentHTML("beforeend", "<div>" + `<i>${days[i]}</i>` + "</div>") :
-                    (i === 6) ? document.body.insertAdjacentHTML("beforeend", "<div>" + `<i>${days[i]}</i>` + "</div>") :
-                        (i !== 5 || i !== 6) ? document.body.insertAdjacentHTML("beforeend", "<div>" + days[i] + "</div>") :
+    (i === dayOfWeek - 1 && i === 5) ?
+        document.body.insertAdjacentHTML("beforeend", "<div><b>" + `<i>${days[i]}</i>` + "</b></div>") :
+        (i === dayOfWeek - 1 && i === 6) ?
+            document.body.insertAdjacentHTML("beforeend", "<div><b>" + `<i>${days[i]}</i>` + "</b></div>") :
+            (i === dayOfWeek - 1 && (i !== 5 && i !== 6)) ?
+                document.body.insertAdjacentHTML("beforeend", "<div><b>" + days[i] + "</b></div>") :
+                (i === 5) ?
+                    document.body.insertAdjacentHTML("beforeend", "<div>" + `<i>${days[i]}</i>` + "</div>") :
+                    (i === 6) ?
+                        document.body.insertAdjacentHTML("beforeend", "<div>" + `<i>${days[i]}</i>` + "</div>") :
+                        (i !== 5 || i !== 6) ?
+                            document.body.insertAdjacentHTML("beforeend", "<div>" + days[i] + "</div>") :
                             ''
 }
