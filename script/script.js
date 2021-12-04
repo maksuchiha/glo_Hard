fe3e1d11c6c15fc21a0fb5ad1aaaf6cea0df1828
Maksim Sheko <uchihaboy95@gmail.com>
2021-12-05 02:03:36 +0300

    refactor days in lesson07

--- script/script.js
+++ script/script.js
@@ -1,36 +1,15 @@
-"use strict"
+"use strict";
 
-const days = [ 'Понедельник', 'Вторник', 'Среда', 'Четверг', 'Пятница', 'Суббота', 'Воскресенье' ]
-const body = document.querySelector('.div')
-const cartElem = document.createElement('div')
-const week = days.join('<br>')
+let days = ["Понедельник", "Вторник", "Среда", "Четверг", "Пятница", "Суббота", "Воскресенье"]
 
-
-cartElem.classList.add('week')
-
-cartElem.innerHTML = `
-                <span class="mon">${days[0]}</span><br>
-                <span class="tue">${days[1]}</span><br>
-                <span class="wed">${days[2]}</span><br>
-                <span class="thu">${days[3]}</span><br>
-                <span class="fri">${days[4]}</span><br>
-                <i class="sat">${days[5]}</i><br>
-                <i class="sun">${days[6]}</i>
-            `
-body.append(cartElem)
-
-if (new Date().getDay() === 1) {
-    cartElem.querySelector('.mon').style.fontWeight = 'bold'
-} else if (new Date().getDay() === 2) {
-    cartElem.querySelector('.tue').style.fontWeight = 'bold'
-} else if (new Date().getDay() === 3) {
-    cartElem.querySelector('.wed').style.fontWeight = 'bold'
-} else if (new Date().getDay() === 4) {
-    cartElem.querySelector('.thu').style.fontWeight = 'bold'
-} else if (new Date().getDay() === 5) {
-    cartElem.querySelector('.fri').style.fontWeight = 'bold'
-} else if (new Date().getDay() === 6) {
-    cartElem.querySelector('.sat').style.fontWeight = 'bold'
-} else if (new Date().getDay() === 0) {
-    cartElem.querySelector('.sun').style.fontWeight = 'bold'
+let dayOfWeek = new Date().getDay()
+if (dayOfWeek === 0) dayOfWeek = 7
+for (let i = 0; i < days.length; i++) {
+    (i === dayOfWeek - 1 && i === 5) ? document.body.insertAdjacentHTML("beforeend", "<div><b>" + `<i>${days[i]}</i>` + "</b></div>") :
+        (i === dayOfWeek - 1 && i === 6) ? document.body.insertAdjacentHTML("beforeend", "<div><b>" + `<i>${days[i]}</i>` + "</b></div>") :
+            (i === dayOfWeek - 1 && (i !== 5 && i !== 6)) ? document.body.insertAdjacentHTML("beforeend", "<div><b>" + days[i] + "</b></div>") :
+                (i === 5) ? document.body.insertAdjacentHTML("beforeend", "<div>" + `<i>${days[i]}</i>` + "</div>") :
+                    (i === 6) ? document.body.insertAdjacentHTML("beforeend", "<div>" + `<i>${days[i]}</i>` + "</div>") :
+                        (i !== 5 || i !== 6) ? document.body.insertAdjacentHTML("beforeend", "<div>" + days[i] + "</div>") :
+                            ''
 }
