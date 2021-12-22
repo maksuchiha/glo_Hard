ea4bb346bbac59b81c9522c38890bbfb8aa4ad3d
Maksim Sheko <uchihaboy95@gmail.com>
2021-12-22 20:36:20 +0300

    feature timeOut in script

--- script/script.js
+++ script/script.js
@@ -1,74 +1,17 @@
 'use strict'
 
-const hard = () => {
-    const lesson02 = (item) => {
-        const num = item,
-            output = [],
-            sNumber = num.toString();
-        for (let i = 0, len = sNumber.length; i < len; i++) {
-            output.push(+sNumber.charAt(i))
-        }
-        const multiple = output.reduce((acc, rec) => acc * rec) ** 3
-        return  String(multiple).slice(0,2)
+const input = document.getElementById('input')
+const output = document.getElementById('p')
+let timeout
+
+
+function outputTimeout() {
+    if (timeout) {
+        clearTimeout(timeout)
     }
-
-    console.log(lesson02(266219))
-
-    const lesson03 = () => {
-        const lang = prompt('Введите либо "ru" либо "en"');
-        const days = new Map([
-            ['en', ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday'] ],
-            ['ru', ['Понедельник', 'Вторник', 'Среда', 'Четверг', 'Пятница', 'Суббота', 'Воскресенье'] ]
-        ])
-
-        if (lang === 'en') {
-            console.log(days.get('en'))
-        }   else if (lang === 'ru') {
-            console.log(days.get('ru'))
-        }   else {
-            console.log('ошибка')
-        }
-
-        switch(lang) {
-            case 'en':
-                console.log(days.get('en'))
-                break;
-
-            case 'ru':
-                console.log(days.get('ru'))
-                break;
-
-            default:
-                console.log('ошибка')
-        }
-
-        console.log(days.get('en'))
-
-        let namePerson = prompt('Введите имя')
-
-        namePerson === 'Артём' ?
-            console.log('директор') :
-            namePerson === 'Александр' ?
-                console.log('преподаватель') :
-                console.log('студент')
-    }
-
-    lesson03()
-
-    const lesson04 = (item) => {
-        if (typeof item !== "string") {
-            return 'передана не строка'
-        } else if (item.length > 30) {
-            item = item.trim()
-            return String(item).slice(0,30) + '...'
-        } else {
-            item = item.trim()
-            return item
-        }
-    }
-
-
-    console.log(lesson04(' 123321123321112332112332111233211233211 '))
+    timeout = setTimeout(() => {
+        output.textContent = input.value
+    }, 300)
 }
 
-hard()
+input.addEventListener('input', outputTimeout)
