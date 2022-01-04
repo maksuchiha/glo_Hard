d8f591cf53a06adbf404d30a8b772dad96267355
Maksim Sheko <uchihaboy95@gmail.com>
2022-01-05 00:41:32 +0300

    feature setData xhr in script

--- script/script.js
+++ script/script.js
@@ -1,29 +1,26 @@
 'use strict'
 
 const hard = () => {
-    const task1 = document.getElementById('task1')
-    const task2 = document.getElementById('task2')
 
+    const getData = () => {
+        fetch('./data/db.json')
+            .then(data => data.json())
+            .then(newData => sendData(newData))
+            .catch(error => console.log(error))
+    }
 
-    task1.innerHTML = task1.innerHTML.replace(/функц\S*/gi, (str) => {
-        return `<strong>${str}</strong>`
-    })
+    const sendData = (data) => {
+        const xhr = new XMLHttpRequest()
 
-    task2.innerHTML = task2.innerHTML.replace(/[\d]+:[\d]+/gi, (str) => {
-        return `<b>${str}</b>`
-    })
+        const body = JSON.stringify(data)
 
-    document.body.innerHTML = document.body.innerHTML.replace(/("(?!")[^"]*")|<[^<>]+>/gi, (str) => {
-        return `<mark>${str}</mark>`
-    })
+        xhr.open("POST", 'https://jsonplaceholder.typicode.com/posts')
+        xhr.setRequestHeader('Content-Type', 'application/json')
 
-    document.body.innerHTML = document.body.innerHTML.replace(/(http?:\/\/[^\s]+)/gi, (str) => {
-        return `<a href="' + url + '">${str}</a>`
-    })
+        xhr.send(body);
+    }
 
-    document.body.innerHTML = document.body.innerHTML.replace(/#([A-F]{3}){1,2}/g, (str) => {
-        console.log(str)
-    })
+    getData()
 
 }
 
