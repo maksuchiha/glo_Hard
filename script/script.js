8401293e1d1003c347673524ed3066af5bec3ca3
Maksim Sheko <uchihaboy95@gmail.com>
2022-01-11 05:08:23 +0300

    feature converter in script

--- script/script.js
+++ script/script.js
@@ -1,46 +1,75 @@
 'use strict'
 
 const hard = () => {
-    const select = document.getElementById('cars')
-    const output = document.querySelector('.output')
+    const select = document.getElementById('select')
+    const valuteInput = document.getElementById('valute')
+    const valuteInfo = document.querySelector('.valuteInfo')
+    const rubInput = document.getElementById('rub')
+    const rubInfo = document.querySelector('.rubInfo')
+    const form = document.getElementById('form')
+    let valute
 
+    fetch('https://www.cbr-xml-daily.ru/daily_json.js')
+        .then(data => data.json())
+        .then(newData => convert(newData))
+        .catch(err => console.log(err))
 
-    const getData = () => {
-        fetch('https://bfs01.getcourse.ru/public/files/12250/88/84120897322424565eb4cddeea2b910a.json?e=1641790799&s=_QUeCM_T1jQ_bpKjfjPHUw')
-            .then(data => data.json())
-            .then(newData => render(newData))
-            .catch(error => console.log(error))
-    }
+    const convert = (data) => {
+        const dollar = data.Valute.USD.Value
+        const euro = data.Valute.EUR.Value
 
-    const out = (e, data) => {
-        const createTextItem = select.querySelectorAll('option')
-
-        createTextItem.forEach((item, textIndex) => {
-            if (+e.target.value === textIndex - 1) {
-                output.innerHTML = `тачка: ${data.cars[textIndex -1].brand} ${data.cars[textIndex -1].model}<br>
-                                      цена: ${data.cars[textIndex -1].price}
-                                    `
-            } else if (e.target.value === 'def') {
-                output.innerHTML = ``
+        select.addEventListener('change', () => {
+            if (select.value === 'usd') {
+                valuteInfo.textContent = 'USD'
+                rubInfo.textContent = 'Российский рубль'
+                rubInput.value = ''
+                valuteInput.value = ''
+            } else if (select.value === 'eur') {
+                valuteInfo.textContent = 'EUR'
+                rubInfo.textContent = 'Российский рубль'
+                rubInput.value = ''
+                valuteInput.value = ''
+            } else {
+                valuteInfo.textContent = ''
+                rubInfo.textContent = ''
+                rubInput.value = ''
+                valuteInput.value = ''
             }
         })
-    }
 
-    const render = (data) => {
-        data.cars.forEach((item, index) => {
-            const option = document.createElement('option')
+        const resetInput = (e, input) => {
+            if (e.target.value.trim() !== '') {
+                input.value = ''
+            }
+        }
 
-            option.textContent = `${item.brand}`
-            option.value = `${index}`
-            select.append(option)
+        const converter = () => {
+            if (select.value !== '' && (rubInput.value.trim() !== '' || valuteInput.value.trim() !== '')) {
+                if (valuteInfo.textContent === 'USD') {
+                    valute = +dollar
+                } else if (valuteInfo.textContent === 'EUR') {
+                    valute = +euro
+                }
+                if (rubInput.value !== '') {
+                    valuteInput.value = +rubInput.value / valute
+                    valuteInput.value = Math.floor(valuteInput.value * 100) / 100
+                } else {
+                    rubInput.value = (valute * +valuteInput.value)
+                    rubInput.value = Math.floor(rubInput.value * 100) / 100
+                }
+            }
+        }
+
+
+        rubInput.addEventListener('input', (e) => resetInput(e, valuteInput))
+
+        valuteInput.addEventListener('input', (e) => resetInput(e, rubInput))
+
+        form.addEventListener('submit', (e) => {
+            e.preventDefault()
+            converter()
         })
-
-        select.addEventListener('change', (e) => {
-            out(e, data)
-        })
     }
-
-    getData()
 }
 
 hard()
